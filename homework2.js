b9419f9a09e0fa91ff853cdcaabdadd93c3b1a3b
yolchi2023 <123739740+yolchi2023@users.noreply.github.com>
2023-02-24 21:41:27 +0100

    Add files via upload
    yes

---
+++ homework2.js
@@ -0,0 +1,13 @@
+// 1.1
+
+const bill=275;
+ tip=0;
+ totalValue=tip+bill;
+tip=(bill>=50&&bill<=300)?bill*0.15:bill*0.2;
+console.log("tip for the bill "+bill+" is: "+tip.toFixed(2)+" total value is: "+totalValue);
+// 1.2
+console.log("totalValue:"+totalValue);
+console.log(`bill is : ${bill}, and tip for this bill is ${tip.toFixed(2)}, totalValue is: ${tip+bill}`);
+
+
+// 2
---
+++ jsDay1.js
@@ -0,0 +1,9 @@
+const age=function(birthyear){
+    return 2040-birthyear;
+
+}
+console.log(age+ "is current age")
+const yearForRetirement=function(birthyear,name){
+
+}
+console.log(yearForRetirement(2000,"blue"));
